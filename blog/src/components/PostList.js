c61083e54f6f2f1a470cc6ebdd7e1560968d9e47
PontusSven <pontus.svensson92@gmail.com>
2020-10-20 14:45:07 +0200

    redux thunk

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
+import { tsExpressionWithTypeArguments } from "@babel/types";
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>PostList</div>;
   }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
