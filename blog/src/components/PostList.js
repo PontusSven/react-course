13b2c6d48b7008de41684fe5aea47b1ecb245cd1
PontusSven <pontus.svensson92@gmail.com>
2020-10-21 16:19:41 +0200

    reducers

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
-import { tsExpressionWithTypeArguments } from "@babel/types";
 
 class PostList extends React.Component {
   componentDidMount() {
